ca850162f0b35271fb56c1ef31bc01597e9bae9d
naldal <cec7378@gmail.com>
2021-08-13 15:59:29 +0900

    Initial Commit

---
+++ SwiftUI-Tutorial/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  SwiftUI-Tutorial
+//
+//  Created by 송하민 on 2021/08/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
